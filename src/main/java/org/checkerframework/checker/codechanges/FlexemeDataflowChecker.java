7015001f4f0943ba15cf92257f83bf3130fef75f
Thomas Schweizer <tschweiz@cs.washington.edu>
2023-01-31 16:47:59 -0800

    Remove checker class since it's not used

--- src/main/java/org/checkerframework/checker/codechanges/FlexemeDataflowChecker.java
+++
@@ -1,9 +0,0 @@
-package org.checkerframework.checker.codechanges;
-
-import org.checkerframework.common.basetype.BaseTypeChecker;
-
-/**
- * This is the entry point for pluggable type-checking.
- */
-public class FlexemeDataflowChecker extends BaseTypeChecker {
-}
