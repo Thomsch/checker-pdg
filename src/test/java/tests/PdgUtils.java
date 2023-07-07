d775f5179ae56b85bebf44b0e24dc5f578bf318b
Thomas Schweizer <tschweiz@cs.washington.edu>
2023-07-07 10:25:23 -0700

    Document Utils class

--- src/test/java/tests/PdgUtils.java
+++ src/test/java/tests/PdgUtils.java
@@ -10,31 +10,47 @@ import java.util.stream.Collectors;
 
 import static org.junit.Assert.assertTrue;
 
+/**
+ * Utility class testing PDGs.
+ */
 public class PdgUtils {
     /**
      * Asserts that the PDG has exactly the given nodes.
-     * @param expectedNodes The list of expected nodes, given as strings. The order of the nodes does not matter.
-     * @param actualNodes The actual nodes of the PDG.
+     * @param expectedNodes the list of expected nodes, given as strings. The order of the nodes does not matter
+     * @param actualNodes the actual nodes of the PDG
      */
     public static void assertEquals(final Set<String> expectedNodes, final Set<PdgNode> actualNodes) {
         Assert.assertEquals(expectedNodes.stream().sorted().collect(Collectors.toList()), actualNodes.stream().map(PdgNode::toString).sorted().collect(Collectors.toList()));
     }
 
+    /**
+     * Asserts that the PDG has an exact number of edges of the given type.
+     * @param expectedNumberOfEdge the expected number of edges
+     * @param expectedEdgeType the expected  type of the edges
+     * @param actualPdg the actual PDG
+     */
     public static void assertEdgeCount(final int expectedNumberOfEdge, final PdgEdge.Type expectedEdgeType, final MethodPdg actualPdg) {
         final Set<PdgEdge> dataEdges = actualPdg.edges().stream().filter(e -> e.type == expectedEdgeType).collect(Collectors.toUnmodifiableSet());
         Assert.assertEquals(expectedNumberOfEdge, dataEdges.size());
     }
 
     /**
-     * Checks if the graph contains an edge with the given labels.
-     * @param expectedFromLabel the label of the source node
-     * @param expectedToLabel the label of the target node
-     * @return true if the graph contains an edge with the given labels, false otherwise
+     * Checks if the graph contains a control edge with the given labels.
+     * @param expectedFromLabel the expected  label of the source node
+     * @param expectedToLabel the expected  label of the target node
+     * @param actualMethodPdg the actual PDG
      */
     public static void assertContainsEdge(final String expectedFromLabel, final String expectedToLabel, MethodPdg actualMethodPdg) {
         assertContainsEdge(expectedFromLabel, expectedToLabel, PdgEdge.Type.CONTROL, actualMethodPdg);
     }
 
+    /**
+     * Checks if the graph contains an edge with the given labels and type.
+     * @param expectedFromLabel the expected label of the source node
+     * @param expectedToLabel the expected  label of the target node
+     * @param expectedType the expected  type of the edge
+     * @param actualMethodPdg the actual PDG
+     */
     public static void assertContainsEdge(final String expectedFromLabel, final String expectedToLabel, final PdgEdge.Type expectedType, final MethodPdg actualMethodPdg) {
         boolean found = false;
         for (PdgEdge edge : actualMethodPdg.edges()) {
